c11da32b820c5c198b750f8def020baa7027c6ed
chunhunghan <chunhunghan@gmail.com>
2019-07-11 10:20:34 +0800

    add FileMulti

---
+++ plugin.go
@@ -0,0 +1,49 @@
+package file_picker
+
+import (
+	"github.com/go-flutter-desktop/go-flutter"
+	"github.com/go-flutter-desktop/go-flutter/plugin"
+	"github.com/pkg/errors"
+)
+
+const channelName = "file_picker"
+
+type FilePickerPlugin struct{}
+
+var _ flutter.Plugin = &FilePickerPlugin{} // compile-time type check
+
+func (p *FilePickerPlugin) InitPlugin(messenger plugin.BinaryMessenger) error {
+	dialogProvider := dialogProvider{}
+
+	channel := plugin.NewMethodChannel(messenger, channelName, plugin.StandardMethodCodec{})
+	//channel.HandleFunc("openDirectory", p.filePicker(dialogProvider, true))
+	channel.HandleFunc("ANY", p.filePicker(dialogProvider, false))
+	channel.HandleFunc("IMAGE", p.filePicker(dialogProvider, false))
+	channel.HandleFunc("AUDIO", p.filePicker(dialogProvider, false))
+	channel.HandleFunc("VIDEO", p.filePicker(dialogProvider, false))
+
+	return nil
+}
+
+func (p *FilePickerPlugin) filePicker(dialog dialog, isDirectory bool) func(arguments interface{}) (reply interface{}, err error) {
+	return func(arguments interface{}) (reply interface{}, err error) {
+		argsMap := arguments.(map[interface{}]interface{})
+		switch argsMap["multipleSelection"].(bool) {
+		case false:
+			fileDescriptor, _, err := dialog.File("select file", "*", isDirectory)
+			if err != nil {
+				return nil, errors.Wrap(err, "failed to open dialog picker")
+			}
+			return fileDescriptor, nil
+
+		case true:
+			fileDescriptors, _, err := dialog.FileMulti("select files", "*")
+			if err != nil {
+				return nil, errors.Wrap(err, "failed to open dialog picker")
+			}
+			return fileDescriptors, nil
+		}
+
+		return
+	}
+}
