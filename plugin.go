9a3ba380f399b410befd383cda93eee7dead3026
chunhunghan <chunhunghan@gmail.com>
2019-07-11 13:47:18 +0800

    add fmt

--- plugin.go
+++ plugin.go
@@ -1,6 +1,7 @@
 package file_picker_multi
 
 import (
+	"fmt"
 	"github.com/go-flutter-desktop/go-flutter"
 	"github.com/go-flutter-desktop/go-flutter/plugin"
 	"github.com/pkg/errors"
@@ -13,8 +14,9 @@ type FilePickerPlugin struct{}
 var _ flutter.Plugin = &FilePickerPlugin{} // compile-time type check
 
 func (p *FilePickerPlugin) InitPlugin(messenger plugin.BinaryMessenger) error {
+	fmt.Println("InitPlugin")
 	dialogProvider := dialogProvider{}
-
+    
 	channel := plugin.NewMethodChannel(messenger, channelName, plugin.StandardMethodCodec{})
 	//channel.HandleFunc("openDirectory", p.filePicker(dialogProvider, true))
 	channel.HandleFunc("ANY", p.filePicker(dialogProvider, false))
@@ -27,6 +29,7 @@ func (p *FilePickerPlugin) InitPlugin(messenger plugin.BinaryMessenger) error {
 
 func (p *FilePickerPlugin) filePicker(dialog dialog, isDirectory bool) func(arguments interface{}) (reply interface{}, err error) {
 	return func(arguments interface{}) (reply interface{}, err error) {
+		fmt.Println("filePicker")
 		fileDescriptor, _, err := dialog.File("select file", "*", isDirectory)
 		if err != nil {
 			return nil, errors.Wrap(err, "failed to open dialog picker")
