ee008cb7f4870c35ebd5e70f6c9dd2ebb9cec695
chunhunghan <chunhunghan@gmail.com>
2019-07-23 14:50:59 +0800

    refactor

--- plugin.go
+++ plugin.go
@@ -17,71 +17,83 @@ var _ flutter.Plugin = &FilePickerPlugin{} // compile-time type check
 
 func (p *FilePickerPlugin) InitPlugin(messenger plugin.BinaryMessenger) error {
 	fmt.Println("InitPlugin")
-	dialogProvider := dialogProvider{}
+	//dialogProvider := dialogProvider{}
 
 	channel := plugin.NewMethodChannel(messenger, channelName, plugin.StandardMethodCodec{})
 	//channel.HandleFunc("openDirectory", p.filePicker(dialogProvider, true))
-	channel.HandleFunc("ANY", p.filePicker(dialogProvider, false, "*"))
-	channel.HandleFunc("IMAGE", p.filePicker(dialogProvider, false, "*"))
-	channel.HandleFunc("AUDIO", p.filePicker(dialogProvider, false, "*"))
-	channel.HandleFunc("VIDEO", p.filePicker(dialogProvider, false, "*"))
-	channel.CatchAllHandleFunc(p.fallBack)
+	/*
+		channel.HandleFunc("ANY", p.filePicker(dialogProvider, false, "*"))
+		channel.HandleFunc("IMAGE", p.filePicker(dialogProvider, false, "*"))
+		channel.HandleFunc("AUDIO", p.filePicker(dialogProvider, false, "*"))
+		channel.HandleFunc("VIDEO", p.filePicker(dialogProvider, false, "*"))
+	*/
+	channel.CatchAllHandleFunc(p.handleFilePicker)
 	return nil
 }
 
-func (p *FilePickerPlugin) fallBack(methodCall interface{}) (reply interface{}, err error) {
-	method := methodCall.(plugin.MethodCall)
+func (p *FilePickerPlugin) handleFilePicker(methodCall interface{}) (reply interface{}, err error) {
+	var fileExtension string
 
-	resolveType := strings.Split(method.Method, "__CUSTOM_")
-	fileExtension := resolveType[1]
-	fmt.Println("fallBack fileExtension:" + fileExtension)
+	method := methodCall.(plugin.MethodCall)
+	multipleSelection := method.Arguments.(bool)
+
+	switch method.Method {
+	case "ANY":
+		fileExtension = "*"
+	case "IMAGE":
+		fileExtension = "*"
+	case "AUDIO":
+		fileExtension = "*"
+	case "VIDEO":
+		fileExtension = "*"
+	default:
+		if strings.HasPrefix("method.Method", "__CUSTOM_") {
+			resolveType := strings.Split(method.Method, "__CUSTOM_")
+			fileExtension = resolveType[1]
+			fmt.Println("handleFilePicker fileExtension:" + fileExtension)
+		} else {
+			fileExtension = "*"
+		}
+	}
 
 	dialogProvider := dialogProvider{}
-	p.test()
-	p.filePicker(dialogProvider, false, fileExtension)
+	p.filePicker(dialogProvider, false, fileExtension, multipleSelection)
 	// return the randomized Method Name
 	return method.Method, nil
 }
 
-func (p *FilePickerPlugin) test() {
-	fmt.Println("test success")
-}
+func (p *FilePickerPlugin) filePicker(dialog dialog, isDirectory bool, fileExtension string, multipleSelection bool) (reply interface{}, err error) {
+	fmt.Println("file Picker")
 
-func (p *FilePickerPlugin) filePicker(dialog dialog, isDirectory bool, fileExtension string) func(arguments interface{}) (reply interface{}, err error) {
-	return func(arguments interface{}) (reply interface{}, err error) {
+	switch multipleSelection {
+	case false:
+		fmt.Println("filePicker fileExtension:" + fileExtension)
+		fileDescriptor, _, err := dialog.File("select file", fileExtension, isDirectory)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to open dialog picker")
+		}
+		return fileDescriptor, nil
 
-		fmt.Println("file Picker")
-
-		switch arguments.(bool) {
-		case false:
-			fmt.Println("filePicker fileExtension:" + fileExtension)
-			fileDescriptor, _, err := dialog.File("select file", fileExtension, isDirectory)
-			if err != nil {
-				return nil, errors.Wrap(err, "failed to open dialog picker")
-			}
-			return fileDescriptor, nil
-
-		case true:
-			fileDescriptors, _, err := dialog.FileMulti("select files", fileExtension)
-			if err != nil {
-				return nil, errors.Wrap(err, "failed to open dialog picker")
-			}
-
-			//type []string is not supported by StandardMessageCodec
-			sliceFileDescriptors := make([]interface{}, len(fileDescriptors))
-			for i, file := range fileDescriptors {
-				sliceFileDescriptors[i] = file
-			}
-
-			return sliceFileDescriptors, nil
-
-		default:
-			fileDescriptor, _, err := dialog.File("select file", fileExtension, isDirectory)
-			if err != nil {
-				return nil, errors.Wrap(err, "failed to open dialog picker")
-			}
-			return fileDescriptor, nil
+	case true:
+		fileDescriptors, _, err := dialog.FileMulti("select files", fileExtension)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to open dialog picker")
 		}
 
+		//type []string is not supported by StandardMessageCodec
+		sliceFileDescriptors := make([]interface{}, len(fileDescriptors))
+		for i, file := range fileDescriptors {
+			sliceFileDescriptors[i] = file
+		}
+
+		return sliceFileDescriptors, nil
+
+	default:
+		fileDescriptor, _, err := dialog.File("select file", fileExtension, isDirectory)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to open dialog picker")
+		}
+		return fileDescriptor, nil
 	}
+
 }
